0bc183869dbdd3cc39b9716c21a30e3a720387b1
agustrio1 <susantoagustrio16@gmail.com>
2023-11-25 06:56:06 +0700

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from './components/Header'
+
+type Props = {}
+
+const Home = (props: Props) => {
+  return (
+    <div className='px-5 max-w-[600px] mx-auto'>
+      <Header/>
+    </div>
+  )
+}
+
+export default Home
