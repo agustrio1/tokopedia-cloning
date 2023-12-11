0583234b899434aad1edfc1366ea8d6b1b26072b
agustrio1 <susantoagustrio16@gmail.com>
2023-12-11 21:26:59 +0700

    add animation placehoder in card component before call api

--- app/components/data/Card.tsx
+++ app/components/data/Card.tsx
@@ -21,6 +21,7 @@ type Props = {};
 
 const Card = (props: Props) => {
   const [data, setData] = useState<Product[]>([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
@@ -28,7 +29,6 @@ const Card = (props: Props) => {
         const response = await fetch("/api/products/");
         if (response.ok) {
           const result: ApiResponse = await response.json();
-          console.log(result.data);
           setData(result.data);
         } else {
           console.error(
@@ -39,37 +39,62 @@ const Card = (props: Props) => {
         }
       } catch (err) {
         console.error("Error fetching data:", err);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchData();
   }, []);
+
+  const renderPlaceholderCards = (count: number): JSX.Element[] => {
+    const placeholderCards: JSX.Element[] = [];
+  
+    for (const i of Array(count).keys()) {
+      placeholderCards.push(
+        <div key={`placeholder-${i}`} className="bg-gray-300 p-2 shadow-lg rounded-md animate-pulse mx-2">
+          <div className="w-40 h-40 bg-gray-400 mb-4 items-center mx-auto"></div>
+          <div className="h-4 bg-gray-400 mb-2"></div>
+          <div className="h-4 bg-gray-400 mb-2"></div>
+          <div className="h-4 bg-gray-400"></div>
+        </div>
+      );
+    }
+  
+    return placeholderCards;
+  };
+  
   return (
     <div className="grid grid-cols-2 sm:grid-cols-2 gap-2 max-w-screen-sm p-4 mx-auto mb-8">
-      {data.map((item) => (
-        <div key={item.id} className="bg-white p-2 shadow-lg rounded-md">
-          <img
-            src={item.image}
-            alt={item.name}
-            loading="lazy"
-            className="w-40 h-40 object-cover mb-4 items-center mx-auto"
-          />
-          <p className="text-sm font-bold mb-2 text-center overflow-hidden">
-            {item.name.length > 30 ? (
-              <span title={item.name}>
-                {item.name.substring(0, 17)}...
-              </span>
-            ) : (
-              item.name
-            )}
-          </p>
-          <p className="text-gray-500 mb-2 text-center">{item.category}</p>
-          <p className="text-black font-bold mb-2 text-center text-sm">
-          Rp. {new Intl.NumberFormat("id-ID").format(item.price)}
-          </p>
-        </div>
-      ))}
+      {loading ? (
+       renderPlaceholderCards(6)
+      ): (
+        data.map((item) => (
+          <div key={item.id} className="bg-white p-2 shadow-lg rounded-md">
+            <img
+              src={item.image}
+              alt={item.name}
+              loading="lazy"
+              className="w-40 h-40 object-cover mb-4 items-center mx-auto"
+            />
+            <p className="text-sm font-bold mb-2 text-center overflow-hidden">
+              {item.name.length > 30 ? (
+                <span title={item.name}>
+                  {item.name.substring(0, 17)}...
+                </span>
+              ) : (
+                item.name
+              )}
+            </p>
+            <p className="text-gray-500 mb-2 text-center">{item.category}</p>
+            <p className="text-black font-bold mb-2 text-center text-sm">
+              Rp. {new Intl.NumberFormat("id-ID").format(item.price)}
+            </p>
+          </div>
+        ))
+      )}
     </div>
+       
   );
 };
 
